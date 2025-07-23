8e9d445eb132024778db25b8344af35e08a876c5
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-23 17:51:54 -0400

    remove margin spacing

--- mya-portfolio/src/app/page.tsx
+++ mya-portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./ui/hero";
+import Experience from "./ui/experience";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero></Hero>
+      <Experience></Experience>
     </main>
   );
 }
