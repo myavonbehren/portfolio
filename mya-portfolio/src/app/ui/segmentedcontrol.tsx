6a6894728e836e203a191a01325e9fd9e2b4fae3
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-23 19:29:14 -0400

    create segmented control

--- mya-portfolio/src/app/ui/segmentedcontrol.tsx
+++ mya-portfolio/src/app/ui/segmentedcontrol.tsx
@@ -1,15 +1,48 @@
+"use client";
 import { JSX, useState } from "react";
+import { motion, LayoutGroup } from "framer-motion";
+import styles from "./segmentedcontrol.module.css";
 
 type SegmentedControlProps = {
-    items: Array<string>;
-}
+  items: Array<string>;
+};
 
-export const SegmentedControl = ({ items }: SegmentedControlProps): JSX.Element => {
-    const [activeItem, setActiveItem] = useState(0);
+const SegmentedControl = ({ items }: SegmentedControlProps): JSX.Element => {
+  const [activeItem, setActiveitem] = useState(0);
+  return (
+    <LayoutGroup>
+      <ol className={styles.list}>
+        {items.map((item, i) => {
+          const isActive = i === activeItem;
+          return (
+            <motion.li
+              className={
+                isActive || i === activeItem - 1
+                  ? styles.itemNoDivider
+                  : styles.item
+              }
+              whileTap={isActive ? { scale: 0.95 } : { opacity: 0.6 }}
+              key={item}
+            >
+              <button
+                onClick={() => setActiveitem(i)}
+                type="button"
+                className={styles.button}
+              >
+                {isActive && (
+                  <motion.div
+                    layoutId="SegmentedControlActive"
+                    className={styles.active}
+                  />
+                )}
+                <span className={styles.label}>{item}</span>
+              </button>
+            </motion.li>
+          );
+        })}
+      </ol>
+    </LayoutGroup>
+  );
+};
 
-    return (
-        <div>
-            <div></div>
-        </div>
-    )
-}
+export default SegmentedControl;
