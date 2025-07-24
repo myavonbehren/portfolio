904515bf6a627c1b3f3f58d0f0fdebc9eb00f02c
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-24 19:25:48 -0400

    refactor Experience component to utilize SegmentedControl for dynamic rendering of Work and Education sections

--- mya-portfolio/src/app/_components/experience/experience.tsx
+++ mya-portfolio/src/app/_components/experience/experience.tsx
@@ -1,17 +1,23 @@
 import SegmentedControl from "../segmentedcontrol";
 import Work from "./work";
-import Dpulogo from "../svg/dpulogo";
-import Nulogo from "../svg/nulogo";
+import Education from "./education";
+
+const items = ["Work", "Education"];
+
+const showItem = (item: string) => {
+    if (item == "Work") {
+        return <Work />
+    } else if (item == "Education") {
+        return <Education />
+    }
+}
 
 const Experience = () => {
     return (
-        <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col items-center justify-center gap-2" id="experience">
             <h2 className="fluid-xl font-bold">Experience</h2>
-            <SegmentedControl items={["Work", "Education"]} />
-            <Work></Work>
-            <Dpulogo className="w-6 h-6"></Dpulogo>
-            <Nulogo className="w-6 h-6"></Nulogo>
-
+            <SegmentedControl items={items} activeItem={0} />
+            
         </div>
     )
 }
