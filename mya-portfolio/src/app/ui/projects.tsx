0ab8358c4acc8d3f51c3aacdd4b41cd48eb23017
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-23 21:24:45 -0400

    add Projects component to display project section

---
+++ mya-portfolio/src/app/ui/projects.tsx
@@ -0,0 +1,11 @@
+import SegmentedControl from "./segmentedcontrol";
+
+
+const Projects = () => {
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center gap-2">
+            <h2>Projects</h2>
+        </div>
+    )
+}
+export default Projects;
