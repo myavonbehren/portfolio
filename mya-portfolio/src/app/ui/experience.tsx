a0acc91e5c51fe6ba9ebe6154ee647c7e93f72fd
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-23 21:24:54 -0400

    refactor Experience component layout to use flexbox for improved responsiveness

--- mya-portfolio/src/app/ui/experience.tsx
+++ mya-portfolio/src/app/ui/experience.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 const Experience = () => {
     return (
-        <div>
+        <div className="h-screen w-screen flex flex-col items-center justify-center gap-2">
             <h2>Experience</h2>
             <SegmentedControl items={["Work", "Education"]} />
         </div>
