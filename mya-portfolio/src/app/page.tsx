f5d56802304159c0e8cf54412201477f51e3364f
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-23 21:25:14 -0400

    update Home component layout to include Projects section within a gradient background

--- mya-portfolio/src/app/page.tsx
+++ mya-portfolio/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "./ui/hero";
 import Experience from "./ui/experience";
+import Projects from "./ui/projects";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero></Hero>
-      <Experience></Experience>
+      <div className="bg-gradient">
+        <Experience></Experience><Projects></Projects>
+      </div>
     </main>
   );
 }
