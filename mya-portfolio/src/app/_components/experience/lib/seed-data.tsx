55cd9a2aba8c820fc872f9dbac8e90f43f047acf
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-24 12:07:35 -0400

    add seed data for experience, education, projects, skills, and about sections

---
+++ mya-portfolio/src/app/_components/experience/lib/seed-data.tsx
@@ -0,0 +1,102 @@
+const experience = [
+    {
+        startDate: '2025-04-01',
+        endDate: '2025-07-01',
+        title: 'Software Developer',
+        company: 'DePaul University, Cybersecruity Clinic',
+        description: 'Defined the technology stack for a new cybersecurity training module by analyzing e-learning platforms. Architected the foundational database schema to support a new student and client tracking system by creating detailed UML diagrams to model data relationships.',
+        icon: ''
+    },
+    {
+        startDate: '2023-09-01',
+        endDate: '2025-03-01',
+        title: 'Computer Science Grader',
+        company: 'DePaul University, Jarvis College of Computing and Digital Media',
+        description: 'Provided targeted technical feedback to a cohort of 60+ students by evaluating Python assignments on core concepts like algorithmic structure, loops, and conditional logic. Collaborated with instructors to develop grading rubrics that assessed algorithmic efficiency and code quality.',
+        icon: ''
+    },
+    {
+        startDate: '2023-05-01',
+        endDate: '2024-07-01',
+        title: 'Creative Content Intern',
+        company: 'Northwestern University, Athletic Communications',
+        description: 'Engineered crowd-cheering animations for large-scale videoboards by scripting motion graphics with JavaScript in Adobe After Effects enhancing the live fan experience.',
+        icon: ''
+    },
+    {
+        startDate: '2022-01-01',
+        endDate: '2023-11-01',
+        title: 'Undergraduate Lab Researcher',
+        company: 'Northwestern University, Athletic Communications',
+        description: 'Selected as one of two freshmen for the university’s Robotic Assisted Locomotion Laboratory to contribute to motion control and hardware integration projects. Engineered Arduino-based autonomous car robot with dual control modes (ultrasonic sensor navigation and IR remote control) using TB6612 motor driver for precise movement control.',
+        icon: ''
+    }
+  ]
+  
+const education = [
+    {
+        startDate: '2021-08-01',
+        endDate: '2025-06-01',
+        title: 'Bachelor of Science in Computer Science',
+        company: 'DePaul University',
+        icon: ''
+    }
+]
+
+
+const projects = [
+    {
+        title: 'Watch&Rate iOS Review App',
+        description: 'Engineered a native iOS application using Swift and SwiftUI, connecting to TMDb REST API for fetching and displaying comprehensive TV show data while implementing Core Data for local watchlist/review storage. Designed and built custom, reusable UI components including an interactive star rating system and intuitive user interface within a clean, tab-based navigation architecture.',
+        skills: ['Swift', 'SwiftUI', 'TMDB API', 'URLSession', 'Core Data'],
+        github: 'https://github.com/myavonbehren/iOS-Watch-Rate',
+        live: '',
+        image: ''
+    },
+    {
+        title: 'Watch&Rate',
+        description: 'Created a full-stack TV review platform by developing a REST API with ASP.NET Core and React for frontend. Implemented JWT authentication with OAuth integration and Entity Framework Core for database operations. Designed responsive UI components with Bootstrap including reusable star rating and like button elements.',
+        skills: ['Typescript', 'C#', 'React', 'ASP.NET Core', 'Entity Framework', 'SQLite', 'Bootstrap', 'Azure', 'Vercel'],
+        github: 'https://github.com/myavonbehren/Watch-Rate',
+        live: '',
+        image: ''
+    },
+    {
+        title: 'The GoStem Employee Website',
+        description: 'Developed responsive React frontend components for a nonprofit employee management system within a 10-person agile development team using Git version control, code reviews, and sprint planning methodologies. Implemented a scheduling interface using FullCalendar with customized views and recurring event handling.',
+        skills: ['JavaScript', 'CSS', 'React', 'FullCalendar'],
+        github: 'https://github.com/myavonbehren/GoStem-Employee-Website',
+        live: '',
+        image: ''
+    },
+    {
+        title: 'NAO Obstacle Detection',
+        description: 'Developed autonomous navigation system for NAO humanoid robot using dual ultrasonic sensors and averaging algorithms to eliminate erratic readings and improve reliability. Built real-time obstacle detection with 1.82-foot safety threshold, 15-degree avoidance maneuvers, integrated OpenCV for colored object detection, and ALMemory service integration for sensor data processing.',
+        skills: ['Python', 'OpenCV', 'NAOqi SDK'],
+        github: 'https://github.com/myavonbehren/NAO-ObstacleDetection',
+        live: '',
+        image: ''
+    },
+]
+
+export const skills = {
+  languages: [
+    'Python', 'Java', 'C/C++', 'C#', 'Kotlin', 'Swift', 'JavaScript', 'Typescript', 'HTML/CSS', 'SQL'
+  ],
+  frameworksAndLibraries: [
+    'React', 'ASP.NET Core', 'Entity Framework Core', 'Bootstrap', 'OpenCV', 'NAOq'
+  ],
+  developerToolsAndPlatforms: [
+    'Git', 'Linux', 'REST APIs', 'SQL Server', 'SQLite', 'Figma', 'Arduino', 'FullCalendar'
+  ],
+  design: [
+    'Photoshop', 'Illustrator', 'After Effects', 'Premiere Pro', 'Maya', 'Nuke'
+  ]
+};
+
+export const about = {
+    name: "Mya Von Behren",
+    description: "I'm a recent summa cum laude Computer Science graduate from DePaul University, with a minor in Visual Effects. This unique background empowers me to build applications that are not just functional, but also visually striking—a 'picture-perfect' blend of robust engineering and creative design. With a strong foundation in algorithms, data structures, and systems design, I'm driven to craft innovative and intuitive applications, whether I'm building responsive user interfaces, robust backend services, or seamless mobile experiences.",
+    image: ''
+}
+
