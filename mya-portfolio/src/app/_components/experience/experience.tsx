4650bd5720cd06cbf120d26b985a3496803080a8
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-24 14:35:59 -0400

    add custom logos to test

--- mya-portfolio/src/app/_components/experience/experience.tsx
+++ mya-portfolio/src/app/_components/experience/experience.tsx
@@ -1,5 +1,7 @@
 import SegmentedControl from "../segmentedcontrol";
 import Work from "./work";
+import Dpulogo from "../svg/dpulogo";
+import Nulogo from "../svg/nulogo";
 
 const Experience = () => {
     return (
@@ -7,6 +9,8 @@ const Experience = () => {
             <h2 className="fluid-xl font-bold">Experience</h2>
             <SegmentedControl items={["Work", "Education"]} />
             <Work></Work>
+            <Dpulogo className="w-6 h-6"></Dpulogo>
+            <Nulogo className="w-6 h-6"></Nulogo>
 
         </div>
     )
