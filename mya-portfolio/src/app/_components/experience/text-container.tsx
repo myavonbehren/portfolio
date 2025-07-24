aadee016d1b80d9b729e73602cb856d72c4ba9fc
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-24 16:40:16 -0400

    implement TextContainer component to fetch and display work experience data from the database using Prisma

--- mya-portfolio/src/app/_components/experience/text-container.tsx
+++ mya-portfolio/src/app/_components/experience/text-container.tsx
@@ -1,7 +1,23 @@
-const TextContainer = () => {
+import prisma from "@/app/lib/db"
+
+const formatDate = (date: Date) => {
+    return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long' })
+}   
+
+const TextContainer = async () => {
+    const works = await prisma.works.findMany()
+
     return (
-        <div>
-            
-        </div>
+        <>
+            {works.map((work) => (
+                <div className="inline-flex flex-col gap-2">
+                    <p>{formatDate(work.startDate)} - {formatDate(work.endDate)}</p>
+                    <p>{work.title}</p>
+                    <p>{work.company}</p>
+                    <p>{work.description}</p>
+                </div>
+            ))}
+        </>
     )
 }
+export default TextContainer;
