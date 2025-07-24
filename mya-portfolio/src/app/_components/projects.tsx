f69ee90a9b8d832680fe1ca21228887e10480aef
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-24 10:21:43 -0400

    remove fixed height and width from Projects component for improved layout flexibility

--- mya-portfolio/src/app/_components/projects.tsx
+++ mya-portfolio/src/app/_components/projects.tsx
@@ -3,7 +3,7 @@ import SegmentedControl from "./segmentedcontrol";
 
 const Projects = () => {
     return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col items-center justify-center gap-2">
             <h2>Projects</h2>
         </div>
     )
