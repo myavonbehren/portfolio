f4f47c0ed48dbb3d9ee7f0f3b5d3d9464c3c2d22
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-25 19:18:42 -0400

    update AboutItem component to support light and dark portrait images

--- mya-portfolio/src/app/_components/about/about-item.tsx
+++ mya-portfolio/src/app/_components/about/about-item.tsx
@@ -1,7 +1,6 @@
 import { fetchAbout } from "@/app/lib/data";
 import Image from "next/image";
 
-
 const AboutItem = async () => {
     const about = await fetchAbout();
     
@@ -10,7 +9,23 @@ const AboutItem = async () => {
             <h1 className="fluid-l font-bold">Hello, I'm Mya 👋🏽</h1>
             {about.map((details: any) => (
                 <div key={details.id} className="flex flex-row gap-2 mt-2">
-                    <Image src={details.image} alt="Mya" width={2000} height={2571} className="block hidden md:block w-2/7 h-2/7 transition delay-10 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105" />
+                    {/* Portrait image container */}
+                    <div className="hidden md:block transition delay-10 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105">
+                        <Image 
+                            src="/portrait-dark.png" 
+                            alt="Mya" 
+                            width={2000} 
+                            height={2571} 
+                            className="portrait-image-dark" 
+                        />
+                        <Image 
+                            src="/portrait-light.png" 
+                            alt="Mya" 
+                            width={2000} 
+                            height={2571} 
+                            className="portrait-image-light" 
+                        />
+                    </div>
                     <p className="fluid-m mt-1 md:mt-6"> {details.description}</p>
                 </div>
             ))}
