70f42bc3eaefbb11e345510f6287a4b255ce45c4
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-24 21:30:16 -0400

    implement API endpoint to fetch works from the database and update db.ts for improved Prisma client handling

---
+++ mya-portfolio/src/app/api/info.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../lib/db';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const works = await prisma.works.findMany();
+  res.status(200).json(works);
+}
--- mya-portfolio/src/app/lib/db.ts
+++ mya-portfolio/src/app/lib/db.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from "../../../generated/prisma"
 
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+  };
 
-
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
-
-const prisma = globalForPrisma.prisma || new PrismaClient()
+  export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
