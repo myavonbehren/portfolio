25f1e808619e618be757760fe7de6959dc2dbe11
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-25 01:14:55 -0400

    refactor ProjectItems component to enhance project display with skills and links, and update Projects component to integrate ProjectItems

--- mya-portfolio/src/app/_components/projects/project-items.tsx
+++ mya-portfolio/src/app/_components/projects/project-items.tsx
@@ -1,15 +1,66 @@
-import { fetchProjects } from "@/app/lib/data";
+import { fetchProjects  } from "@/app/lib/data";
+import LogoDisplay from "../experience/logodisplay";
+import { FiGithub } from "react-icons/fi";
+import { TbWorldWww } from "react-icons/tb";
 
-const formatDate = (date?: Date | null) => {
-    if (!date) return "Present";
+const formatDate = (date: Date) => {
     return new Date(date).toLocaleDateString('en-US', { year: 'numeric' });
   };
 
+const skills = (skills: any) => {
+    return skills.map((skill: any, index: number) => (
+        <button className="fluid-s btn-base px-3 py-2" key={index}>{skill}</button>
+    ))
+}
 
-const ProjectItems = () => {
+const live = (live: string) => {
+    if (!live) {
+        return null;
+    }
     return (
-        <div >
-            <h1>ProjectItems</h1>
+        <a
+        href={live}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn-base p-1.5 inline-flex items-center transition delay-10 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">                
+            <TbWorldWww className="w-3.5 h-3.5"/>
+        </a>
+    )
+}
+
+const ProjectItems = async () => {
+    const projects = await fetchProjects();
+
+    return (
+        <div className="grid grid-cols-3 gap-4">
+        {projects.map((project: any) => (
+          <div className="container-base" key={project.id}>
+            <LogoDisplay icon={project.icon} />
+            <div>
+              <div>
+                <div className="self-stretch inline-flex justify-between items-center">
+                    <p className="fluid-m">{project.title}</p>
+                    
+                    {live(project.live)}
+
+                    <a
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-base p-1.5 inline-flex items-center transition delay-10 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">                
+                        <FiGithub className="w-3.5 h-3.5"/>
+                    </a>
+
+                </div>
+
+                <div className="flex flex-row gap-2">
+                    {skills(project.skills)}
+                </div>
+
+              </div>
+            </div>
+          </div>
+        ))}
         </div>
     )
 }
--- mya-portfolio/src/app/_components/projects/projects.tsx
+++ mya-portfolio/src/app/_components/projects/projects.tsx
@@ -1,10 +1,11 @@
+import ProjectItems from "./project-items";
 
 const Projects = () => {
     return (
-        <div className="flex flex-col justify-start pt-10" id="experience">
+        <div className="flex flex-col justify-start pt-10" id="projects">
         <h1 className="fluid-xxl font-bold">Projects</h1>
         <div className="w-full justify-center items-center">
-            <p>test</p>
+            <ProjectItems />
         </div>
     </div>
     )
