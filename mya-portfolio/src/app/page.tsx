213491a511dec6d6dd236e4071a74a92f81d8eb4
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-25 03:37:23 -0400

    add about component

--- mya-portfolio/src/app/page.tsx
+++ mya-portfolio/src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./_components/hero";
 import Experience from "./_components/experience/experience";
 import Projects from "./_components/projects/projects";
 import Container from "./_components/container";
+import About from "./_components/about";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
           <Container>
             <Experience></Experience>
             <Projects></Projects>
+            <About></About>
           </Container>
         </div>
     </main>
