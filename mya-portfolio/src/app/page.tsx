22138f2e82a4bc6802156487549b1209449d6faa
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-25 04:00:22 -0400

    update import path for About component to include subdirectory

--- mya-portfolio/src/app/page.tsx
+++ mya-portfolio/src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./_components/hero";
 import Experience from "./_components/experience/experience";
 import Projects from "./_components/projects/projects";
 import Container from "./_components/container";
-import About from "./_components/about";
+import About from "./_components/about/about";
 
 export default function Home() {
   return (
