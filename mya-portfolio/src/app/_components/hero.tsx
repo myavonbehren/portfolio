225982c68ce0f06a2275e123a8e35f7f11b93781
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-25 23:36:53 -0400

    Add resume PDF and update Hero component to link to it with an arrow icon

--- mya-portfolio/src/app/_components/hero.tsx
+++ mya-portfolio/src/app/_components/hero.tsx
@@ -1,7 +1,7 @@
 import { FiLinkedin } from "react-icons/fi";
 import { FiGithub } from "react-icons/fi";
 import { FiMail } from "react-icons/fi";
-
+import { FiArrowRight } from "react-icons/fi";
 
 const Hero = () => {
   return (
@@ -10,9 +10,16 @@ const Hero = () => {
         <h1 className="font-bold uppercase fluid-xxxl text-center">Full Stack <br /> Developer</h1>
         <h3 className="font-bold fluid-l text-center">Based near Asheville, NC</h3>
         <div className="flex items-center gap-3 pt-3 justify-center">
-            <button className="btn-base px-5 py-2.5 font-bold transition delay-10 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
-                Resume
-            </button>
+            <a 
+            href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:2915f675-d318-4d77-887d-c2099d368b29"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-base px-5 py-2.5 font-bold transition delay-10 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+                <div className="flex items-center gap-2">
+                    Resume
+                    <FiArrowRight className="w-4 h-4"/>
+                </div>
+            </a>
             <a
             href="https://www.linkedin.com/in/myavonbehren"
             target="_blank"
