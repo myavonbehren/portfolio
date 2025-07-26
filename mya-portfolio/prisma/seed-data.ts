1bf0e9013173666202f9a71dcdff9066bfc9130e
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-25 21:42:34 -0400

    update vercel-build script in package.json to include Prisma migration deployment

---
+++ mya-portfolio/prisma/seed-data.ts
@@ -0,0 +1,109 @@
+export const works = [
+    {
+        startDate: '2025-04-01',
+        endDate: '',
+        title: 'Software Developer',
+        company: 'DePaul University, Cybersecruity Clinic',
+        description: 'Defined the technology stack for a new cybersecurity training module by analyzing e-learning platforms. *Architected the foundational database schema to support a new student and client tracking system by creating detailed UML diagrams to model data relationships.',
+        icon: 'dpulogo'
+    },
+    {
+        startDate: '2023-09-01',
+        endDate: '2025-03-01',
+        title: 'Computer Science Grader',
+        company: 'DePaul University, Jarvis College of Computing and Digital Media',
+        description: 'Provided targeted technical feedback to a cohort of 60+ students by evaluating Python assignments on core concepts like algorithmic structure, loops, and conditional logic. *Collaborated with instructors to develop grading rubrics that assessed algorithmic efficiency and code quality.',
+        icon: 'dpulogo'
+    },
+    {
+        startDate: '2023-05-01',
+        endDate: '2024-07-01',
+        title: 'Creative Content Intern',
+        company: 'Northwestern University, Athletic Communications',
+        description: 'Engineered crowd-cheering animations for large-scale videoboards by scripting motion graphics with JavaScript in Adobe After Effects enhancing the live fan experience.',
+        icon: 'nulogo'
+    },
+    {
+        startDate: '2022-01-01',
+        endDate: '2023-11-01',
+        title: 'Undergraduate Lab Researcher',
+        company: 'Northwestern University, Athletic Communications',
+        description: 'Selected as one of two freshmen for the university’s Robotic Assisted Locomotion Laboratory to contribute to motion control and hardware integration projects. *Engineered Arduino-based autonomous car robot with dual control modes (ultrasonic sensor navigation and IR remote control) using TB6612 motor driver for precise movement control.',
+        icon: 'dpulogo'
+    }
+  ]
+  
+export const education = [
+    {
+        startDate: '2021-08-01',
+        endDate: '2025-06-01',
+        title: 'Bachelor of Science in Computer Science',
+        company: 'DePaul University',
+        icon: 'dpulogo'
+    }
+]
+
+
+export const projects = [
+    {
+        title: 'Watch&Rate iOS Review App',
+        description: 'TV show review and watchlist app for iOS using TMDB.',
+        skills: ['Swift', 'SwiftUI', 'TMDB API', 'URLSession', 'Core Data'],
+        github: 'https://github.com/myavonbehren/iOS-Watch-Rate',
+        live: 'https://watch-rate.vercel.app/',
+        image: '',
+        startDate: '2025-03-01',
+        endDate: '2025-06-01'
+    },
+    {
+        title: 'Watch&Rate',
+        description: 'Full stack application to review TV shows and add to a personal watchlist.',
+        skills: ['Typescript', 'C#', 'React', 'ASP.NET Core', 'Entity Framework', 'SQLite', 'Bootstrap', 'Azure', 'Vercel'],
+        github: 'https://github.com/myavonbehren/Watch-Rate',
+        live: '',
+        image: '',
+        startDate: '2025-03-01',
+        endDate: '2025-06-01'
+    },
+    {
+        title: 'The GoStem Employee Website',
+        description: 'Internal GoSTEM application for scheduling, notes, and communication.',
+        skills: ['JavaScript', 'CSS', 'React', 'FullCalendar'],
+        github: 'https://github.com/myavonbehren/GoStem-Employee-Website',
+        live: '',
+        image: '',
+        startDate: '2025-01-01',
+        endDate: '2025-03-01'
+    },
+    {
+        title: 'NAO Obstacle Detection',
+        description: 'An autonomous navigation system for Aldebaran’s NAO humanoid robot built at DePaul’s RAL Lab.',
+        skills: ['Python', 'OpenCV', 'NAOqi SDK'],
+        github: 'https://github.com/myavonbehren/NAO-ObstacleDetection',
+        live: '',
+        image: '',
+        startDate: '2023-09-01',
+        endDate: '2023-11-01'
+    },
+]
+
+export const skills = {
+  languages: [
+    'Python', 'Java', 'C/C++', 'C#', 'Kotlin', 'Swift', 'JavaScript', 'Typescript', 'HTML/CSS', 'SQL'
+  ],
+  frameworksAndLibraries: [
+    'React', 'ASP.NET Core', 'Entity Framework Core', 'Bootstrap', 'OpenCV', 'NAOq'
+  ],
+  developerToolsAndPlatforms: [
+    'Git', 'Linux', 'REST APIs', 'SQL Server', 'SQLite', 'Figma', 'Arduino', 'FullCalendar'
+  ],
+  design: [
+    'Photoshop', 'Illustrator', 'After Effects', 'Premiere Pro', 'Maya', 'Nuke'
+  ]
+};
+
+export const about = {
+    name: "Mya Von Behren",
+    description: "I'm a recent summa cum laude Computer Science graduate from DePaul University, with a minor in Visual Effects. This unique background empowers me to build applications that are not just functional, but also visually striking—a 'picture-perfect' blend of robust engineering and creative design. With a strong foundation in algorithms, data structures, and systems design, I'm driven to craft innovative and intuitive applications, whether I'm building responsive user interfaces, robust backend services, or seamless mobile experiences.",
+    image: ''
+}
