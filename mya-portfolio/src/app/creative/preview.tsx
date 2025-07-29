647f7767865f40bb7ac22a4d5bae397c18603332
Mya Von Behren <mvonbehr@depaul.edu>
2025-07-28 22:54:34 -0400

    Add Preview component for showcasing creative projects with images and a link to view more

---
+++ mya-portfolio/src/app/creative/preview.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Preview = () => {
+    return (
+        <div className="pt-15">
+            <h1 className="fluid-l font-semibold">Creative Projects</h1>
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 mt-5 pb-3">
+                <div className="aspect-video">
+                    <Image 
+                        src="/LOCK-IT-DOWN-LACROSSE.gif" 
+                        alt="LOCK IT DOWN GIF" 
+                        width={700} 
+                        height={394} 
+                        className="w-full h-full object-cover"
+                        priority
+                        />
+                </div>
+                <div className="aspect-video">
+                    <Image 
+                    src="/Rotoscoping.gif" 
+                    alt="Rotoscoping GIF" 
+                    width={1280} 
+                    height={720} 
+                    className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="aspect-video">
+                    <Image 
+                    src="/GET-LOUD.gif" 
+                    alt="GET LOUD GIF" 
+                    width={700} 
+                    height={394} 
+                    className="w-full h-full object-cover"
+                    />
+                </div>
+            </div>
+            <Link href="/creative" className="cursor-pointer hover:underline" >
+                <h2 className="fluid-m">Show More...</h2>
+            </Link>
+        </div>
+    )
+}
+
+export default Preview;
