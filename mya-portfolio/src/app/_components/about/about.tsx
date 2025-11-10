3ab1868a96239e4a3d80410275a500ccd2b13329
Mya Von Behren <mvonbehr@depaul.edu>
2025-11-10 00:45:15 -0500

    Wrap About section in Container component and add background gradient for improved styling

--- mya-portfolio/src/app/_components/about/about.tsx
+++ mya-portfolio/src/app/_components/about/about.tsx
@@ -1,12 +1,17 @@
 import AboutItem from "./about-item";
+import Container from "../container";
 
 const About = () => {
     return (
+    <div className="bg-hero-gradient">
+    <Container>
     <div className="flex flex-col justify-start py-20" id="about">
         <h1 className="fluid-xxl font-bold">About</h1>
         <div className="w-full justify-center items-center">
             <AboutItem />   
         </div>
+    </div>
+    </Container>
     </div>
     )
 }
