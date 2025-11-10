472e936dfcc8aad9c5527aa8b28375dffff2d735
Mya Von Behren <mvonbehr@depaul.edu>
2025-11-10 00:45:08 -0500

    Wrap Experience section in Container component and add background gradient for enhanced styling

--- mya-portfolio/src/app/_components/experience/experience.tsx
+++ mya-portfolio/src/app/_components/experience/experience.tsx
@@ -2,9 +2,12 @@ import Work from "./work";
 import Education from "./education";
 import ExperienceTabs from "./experience-tabs";
 import Skills from "./skills";
+import Container from "../container";
 
 const Experience = () => {
   return (
+    <div className="bg-experience-gradient">
+    <Container>
     <div className="flex flex-col justify-start pt-20" id="experience">
         <h1 className="fluid-xxl font-bold">Experience</h1>
         <div className="w-full justify-center items-center">
@@ -14,6 +17,8 @@ const Experience = () => {
             </ExperienceTabs>
             <Skills />
         </div>
+    </div>
+    </Container>
     </div>
   );
 };
