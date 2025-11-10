86159ca2770e6b10f272a41db9cd37624cfe8ef7
Mya Von Behren <mvonbehr@depaul.edu>
2025-11-10 01:26:19 -0500

    Refactor Projects section to utilize ProjectsContent component for improved structure and presentation

--- mya-portfolio/src/app/_components/projects/projects.tsx
+++ mya-portfolio/src/app/_components/projects/projects.tsx
@@ -1,15 +1,15 @@
 import ProjectItems from "./project-items";
 import Container from "../container";
+import ProjectsContent from "./projects-content";
 
 const Projects = () => {
     return (
     <div className="bg-projects">
     <Container>
     <div className="flex flex-col justify-start pt-20" id="projects">
-        <h1 className="fluid-xxl font-bold">Projects</h1>
-        <div className="w-full justify-center items-center">
-            <ProjectItems />
-        </div>
+      <ProjectsContent>
+        <ProjectItems />
+      </ProjectsContent>
     </div>
     </Container>
     </div>
