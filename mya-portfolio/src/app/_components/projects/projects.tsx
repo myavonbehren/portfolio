bce18937da73a81cd691a07abfac88b51f0a170d
Mya Von Behren <mvonbehr@depaul.edu>
2025-11-10 00:45:02 -0500

    Add Container component and wrap Projects section for improved layout

--- mya-portfolio/src/app/_components/projects/projects.tsx
+++ mya-portfolio/src/app/_components/projects/projects.tsx
@@ -1,12 +1,17 @@
 import ProjectItems from "./project-items";
+import Container from "../container";
 
 const Projects = () => {
     return (
+    <div className="bg-projects">
+    <Container>
     <div className="flex flex-col justify-start pt-20" id="projects">
         <h1 className="fluid-xxl font-bold">Projects</h1>
         <div className="w-full justify-center items-center">
             <ProjectItems />
         </div>
+    </div>
+    </Container>
     </div>
     )
 }
