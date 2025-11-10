6f770e9f91c18c81310f1908d7a1570ac0b02b56
Mya Von Behren <mvonbehr@depaul.edu>
2025-11-10 01:26:00 -0500

    Refactor About section to utilize AboutContent component for improved structure and presentation

--- mya-portfolio/src/app/_components/about/about.tsx
+++ mya-portfolio/src/app/_components/about/about.tsx
@@ -1,15 +1,15 @@
 import AboutItem from "./about-item";
 import Container from "../container";
+import AboutContent from "./about-content";
 
 const About = () => {
     return (
     <div className="bg-hero-gradient">
     <Container>
     <div className="flex flex-col justify-start py-20" id="about">
-        <h1 className="fluid-xxl font-bold">About</h1>
-        <div className="w-full justify-center items-center">
-            <AboutItem />   
-        </div>
+      <AboutContent>
+        <AboutItem />   
+      </AboutContent>
     </div>
     </Container>
     </div>
